55969a01010fd40fa122d8bb9c15d3341744ab99
John Angel <john.angel@insperity.com>
2019-04-10 20:31:04 -0500

    Added routing to product details.

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,10 +4,10 @@ import { Component } from "@angular/core";
   selector: 'pm-root',
   template: `
     <nav class='navbar navbar-expand navbar-light bg-light'>
-      <a class='navbar-brand'>{{pageTitle}}</a>
+      <a class='brand'>{{pageTitle}}</a>
       <ul class='nav nav-pills'>
-        <li class='active'><a [routerLink]="['/welcome']">Home</a></li>
-        <li><a [routerLink]="['/products']">Product List</a></li>
+        <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+        <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
       </ul>
     </nav>
     <div class='container'>
