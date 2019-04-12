4078b403ce4ab2b26656e442b703f5c450fd63a9
John Angel <john.angel@insperity.com>
2019-04-11 22:16:53 -0500

    Add detail component. Added data to url template.

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,23 +10,38 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = 'Product Detail';
-  product: IProduct;
+  products: IProduct[] = [];
+  errorMessage: string;
+  id: number;
+  foundProduct: IProduct;
+ 
+  constructor(private route: ActivatedRoute, private router: Router,
+              private productService: ProductService) { }
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  _product: IProduct;
+  get product(): IProduct{
+    return this._product;
+  }
+  set product(value: IProduct){
+    this._product=value;
+    this.foundProduct = this.product ? this.findProduct() : this.product;
+  }
+  
+  findProduct(): IProduct {
+    return this.products.filter((item: IProduct) => item.productId == this.id)[0];
+  }
 
-  ngOnInit() {
+  ngOnInit() : void {
     let id = +this.route.snapshot.paramMap.get('id');
+    this.id = id;
     this.pageTitle += `: ${id}`;
-    this.product = {
-      'productId': id,
-      'productName': 'Leaf Rake',
-      'productCode': 'GDN-0011',
-      'releaseDate': 'March 19, 2016',
-      'description': 'Leaf rake with 48-inch wooden handle.',
-      'price': 19.95,
-      'starRating': 3.2,
-      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rage.png'
-    }
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products
+        this.product = this.findProduct();
+      },
+      error => this.errorMessage = <any>error
+    );
   }
 
   onBack(): void{
